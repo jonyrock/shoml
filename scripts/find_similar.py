9a3312553319379bb0a85e3525bcf1be3f5e0bbb
evgentu <evgentu@gmail.com>
2017-04-29 23:32:45 +0300

    implemented find similar script

--- scripts/find_similar.py
+++ scripts/find_similar.py
@@ -1,13 +1,16 @@
 import numpy as np
 import os
 import sys
+import csv
 
 path = sys.argv[1]
 out_path = sys.argv[2]
 
 
-root_dir = ".."
-features_dir = root_dir + "/features"
+root_dir = "../"
+data_dir = root_dir + "shoml_data/"
+csv_file = "athletic_flatten.csv"
+features_dir = root_dir + "features/"
 input_filename = path
 features_filename = path + ".f"
 
@@ -15,10 +18,12 @@ features_filename = path + ".f"
 
 def load_array(filename):
     with open(filename) as file:
-        return np.load(file)
+        res = np.load(file)
+        return np.reshape(res, 25088)
 
 
-os.system("test.py " + input_filename + " " + features_filename)
+
+os.system("python test.py " + input_filename + " " + features_filename)
 
 
 image_array = load_array(features_filename)
@@ -26,11 +31,19 @@ image_array = load_array(features_filename)
 
 result = []
 
-for filename in os.listdir(features_dir):
-    if filename.endswith(".jpg.f"):
-        current_array = load_array(filename)
-        cur_dist = float(np.dot(image_array, current_array)) / np.product(current_array)
-        result.append([cur_dist, filename])
+with open(csv_file) as list_file:
+    reader = csv.reader(csv_file, delimiter=' ')
+    for row in reader:
+        name = row[0]
+        url = row[1]
+        filename = features_dir + name + ".jpg.f"
+        if not os.path.exists(features_dir + filename):
+            continue
+        current_array = load_array(features_dir + filename)
+        mod = np.product(current_array)
+        if mod > 0.:
+            cur_dist = float(np.dot(image_array, current_array)) / np.product(current_array)
+            result.append([cur_dist, url])
 
 result.sort()
 size = min(10, len(result))
