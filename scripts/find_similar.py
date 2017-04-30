935dab3b312fb9881a9effdffc3ee026da04a075
evgentu <evgentu@gmail.com>
2017-04-30 11:40:37 +0300

    write to json

--- scripts/find_similar.py
+++ scripts/find_similar.py
@@ -1,10 +1,10 @@
 import numpy as np
 import os
 import sys
-import csv
+import simplejson as json
 
 path = sys.argv[1]
-out_path = sys.argv[2]
+out_path = path + ".json"
 
 
 root_dir = "../"
@@ -27,27 +27,33 @@ os.system("python test.py " + input_filename + " " + features_filename)
 
 
 image_array = load_array(features_filename)
-print image_array
 
-result = []
+weights = []
 
 with open(csv_file) as list_file:
     for row in list_file.readlines():
-        tokens = row.split(' ')
+        tokens = row.strip().split(' ')
         name = tokens[0]
         url = tokens[1]
         filename = features_dir + name + ".jpg.f"
-        #print(filename)
         if not os.path.exists(filename):
             continue
         current_array = load_array(filename)
         mod = np.sqrt(np.dot(current_array, current_array))
         
         cur_dist = float(np.dot(image_array, current_array)) / mod
-        result.append([cur_dist, name, url])
+        weights.append([cur_dist, name, url])
 
-result.sort()
-result.reverse()
-size = min(10, len(result))
+weights.sort()
+weights.reverse()
+size = min(10, len(weights))
+result = []
 for i in range(size):
-    print(result[i])
+    result.append({
+        'name': weights[i][1],
+        'image': weights[i][2],
+        'weight': weights[i][0],
+    })
+
+with open(out_path, "w+") as file:
+    file.write(json.dumps(result, ensure_ascii=False))
